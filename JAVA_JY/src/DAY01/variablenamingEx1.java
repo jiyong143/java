a2018cba504a180479c3464701cc261a4a5568fe
jiyong143 <153248126+jiyong143@users.noreply.github.com>
2023-12-08 17:15:29 +0900

    java_1일차_변수 명명 문제

---
+++ JAVA_JY/src/DAY01/variablenamingEx1.java
@@ -0,0 +1,33 @@
+package DAY01;
+
+public class variablenamingEx1 {
+
+	//변수명 작성 규칙(필수)과 관례(선탹이지만 추천)
+	public static void main(String[] args) {
+		
+		//1.대소문자 구분하기
+		int num;
+		int NUM;
+		int Num;
+		int nuM;
+		
+		//2.예악어 사용 불가능
+		//int int;
+		//int public;
+		//int class;
+		
+		//3. 숫자로 시작 불가능
+		//int 2num;
+		int num2;
+		
+		//4.특수문자는 _와 $만 가능
+		int _num;
+		//int #num;
+		//int n um;
+		int num$;
+		
+		
+		
+	}
+
+}
