e8d8169fcf5708ec68164966da697eb7da5069bf
jiyong143 <153248126+jiyong143@users.noreply.github.com>
2024-02-26 13:54:14 +0900

    community v.1-cjy

--- servlet/servlet_project2/src/main/java/kr/kh/app/controller/MainServlet.java
+++ servlet/servlet_project2/src/main/java/kr/kh/app/controller/MainServlet.java
@@ -1,4 +1,4 @@
-package controller;
+package kr.kh.app.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,17 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
-	
+       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
 	}
-
-	
 
 }
